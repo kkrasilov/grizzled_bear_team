43955800daf0d760f93a7e4018643ec925faaa97
kkrasilov <kkrasilov@gmail.com>
2025-06-03 10:38:13 +0500

    setup devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
   enum gender: {
     male: 'male',
     female: 'female'
