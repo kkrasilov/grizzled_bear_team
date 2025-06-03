d54b1abaea68adfcd1c88277e985528bb4067b24
kkrasilov <kkrasilov@gmail.com>
2025-06-03 17:16:52 +0500

    add guests page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,26 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :assert_path_allowed
   
   private
+
+  def assert_path_allowed
+    redirect_to guests_path unless token.present?
+  end
+
+  def token
+    cookies[:jwt_token].presence || (token_verification && fetch_token[:value])
+  end
+
+  def token_verification
+    JWT.decode(params[:token], ENV.fetch('JWT_SECRET', nil))
+  rescue StandardError
+    false
+  end
+
+  def fetch_token
+    cookies[:jwt_token] = { value: params[:token], expires: 3.months.from_now }
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[login email first_name surname])
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  get '/guests', to: redirect('/guests.html')
   root "welcome#index"
 end
