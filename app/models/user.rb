4fab8b15543a511f0613b09c270df478a621859d
kkrasilov <kkrasilov@gmail.com>
2025-06-21 18:40:50 +0500

    add active storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-  enum gender: {
+  enum :gender, {
     male: 'male',
     female: 'female'
   }
-  
-  validates :login, uniqueness: true
+
   validates :email, uniqueness: true
-  validates :login, presence: true
   validates :email, presence: true
+
+  has_one_attached :avatar
 
   def short_name
     "#{surname} #{first_name}"
