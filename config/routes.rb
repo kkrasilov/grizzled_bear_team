e93f8e5f2db19b83ca72cc2056ce67258568d62c
kkrasilov <kkrasilov@gmail.com>
2025-06-21 15:58:08 +0500

    add welcome landing page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   get '/guests', to: redirect('/guests.html')
-  root "welcome#index"
+  root 'welcome#index'
+
+  get 'about_team', to: 'articles#about_team', as: :about_team
 end
