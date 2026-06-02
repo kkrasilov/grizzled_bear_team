32eb37d23267db045c186bff3f22665ab5ddf45a
kkrasilov <kkrasilov@gmail.com>
2026-06-02 15:42:11 +0500

    move image links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,12 @@ module ApplicationHelper
     options[:class] += " #{active_class}"
     link_to(name, path, **options, &block)
   end
+
+  def cloud_image_path(image_path)
+    "#{CLOUD_STORAGE_URL}/#{image_path}"
+  end
+
+  def cloud_image_tag(image_path, **options)
+    image_tag cloud_image_path(image_path), **options
+  end
 end
---
+++ config/initializers/cloud_storage.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+CLOUD_STORAGE_URL = 'https://storage.yandexcloud.net/grizzledbearteam/images'
